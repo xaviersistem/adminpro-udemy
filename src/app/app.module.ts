f5a0c69c8c80e871c357448f8923f9eb3e0db0b0
xharo <xharo@yachay.gob.ec>
2019-11-12 17:11:41 -0500

    modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
-// Rutdas
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './share/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './share/header/header.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Modulos
+
+import { PagesModule } from './pages/pages.module';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
-  ],
-  imports: [BrowserModule, APP_ROUTES, AppRoutingModule],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, APP_ROUTES, AppRoutingModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
